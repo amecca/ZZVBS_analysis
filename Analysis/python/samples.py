5dbf5baaa2a6d0010fa6fde03c26dbde8371cc5b
Alberto Mecca <alberto.mecca@cern.ch>
2026-04-02 17:45:43 +0200

    samples: add qqZZ, use q#bar{q} instead of qq

--- Analysis/python/samples.py
+++ Analysis/python/samples.py
@@ -4,18 +4,19 @@ import cmsstyle
 import ROOT
 
 SAMPLE_DICTS = {
-    'qqZZ-EWK': {'title': 'qq #rightarrow ZZjj #rightarrow 4l 2j EWK'    , 'fnames':['ZZTo4l_2Jets_EW']},
-    'qqZZ-int': {'title': 'qq #rightarrow ZZjj #rightarrow 4l 2j interf.', 'fnames':['ZZTo4l_2Jets_EW_QCD']},
-    'qqZZ-QCD': {'title': 'qq #rightarrow ZZjj #rightarrow 4l 2j QCD'    , 'fnames':['ZZTo4l_2Jets_QCD']},
+    'qqZZ-EWK': {'title': 'q#bar{q} #rightarrow ZZjj #rightarrow 4l 2j EWK'    , 'fnames':['ZZTo4l_2Jets_EW']},
+    'qqZZ-int': {'title': 'q#bar{q} #rightarrow ZZjj #rightarrow 4l 2j interf.', 'fnames':['ZZTo4l_2Jets_EW_QCD']},
+    'qqZZ-QCD': {'title': 'q#bar{q} #rightarrow ZZjj #rightarrow 4l 2j QCD'    , 'fnames':['ZZTo4l_2Jets_QCD']},
     'ggZZ'    : {'title': 'gg #rightarrow ZZ #rightarrow 4l MCFM', 'fnames': ['ggTo%s_Contin_MCFM701'%(fs) for fs in ('4e','2e2mu','4mu')]},
     'rare'    : {'title': 'VVZ', 'fnames':['ZZZ','WZZ','WWZ']}, #ttZ "t#bar{t}Z" is missing!
-    'qqZZ-1J' : {'title': 'qq #rightarrow ZZ+1j #rightarrow 4l j'        , 'fnames':['ZZTo4l_1Jets']},
+    'qqZZ-1J' : {'title': 'q#bar{q} #rightarrow ZZ+1j #rightarrow 4l j'        , 'fnames':['ZZTo4l_1Jets']},
+    'qqZZ'    : {'title': 'q#bar{q} #rightarrow ZZ #rightarrow 4l'             , 'fnames':['ZZTo4l']},
 }
 
 DATA = {'name': 'data', 'title': 'Data', 'fnames': ['data'], 'color': ROOT.kBlack}
 
 def get_samples(region: str='4P'):
-    MC_names = ['qqZZ-EWK', 'qqZZ-int', 'qqZZ-QCD', 'ggZZ', 'rare'] #, 'qqZZ-1J']
+    MC_names = ['qqZZ-EWK', 'qqZZ-int', 'qqZZ-QCD', 'ggZZ', 'rare'] #, 'qqZZ', 'qqZZ-1J']
     MCs = [{'name':k, **SAMPLE_DICTS[k]} for k in MC_names]
 
     # AUTOMATIC COLORS #
