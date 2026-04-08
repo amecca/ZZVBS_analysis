f9c53a206d0ef31165966ac5889e474441cbe0ac
Alberto Mecca <alberto.mecca@cern.ch>
2026-04-08 10:53:57 +0200

    samples: remove final state from title

--- Analysis/python/samples.py
+++ Analysis/python/samples.py
@@ -4,13 +4,13 @@ import cmsstyle
 import ROOT
 
 SAMPLE_DICTS = {
-    'qqZZ-EWK': {'title': 'q#bar{q} #rightarrow ZZjj #rightarrow 4l 2j EWK'    , 'fnames':['ZZTo4l_2Jets_EW']},
-    'qqZZ-int': {'title': 'q#bar{q} #rightarrow ZZjj #rightarrow 4l 2j interf.', 'fnames':['ZZTo4l_2Jets_EW_QCD']},
-    'qqZZ-QCD': {'title': 'q#bar{q} #rightarrow ZZjj #rightarrow 4l 2j QCD'    , 'fnames':['ZZTo4l_2Jets_QCD']},
-    'ggZZ'    : {'title': 'gg #rightarrow ZZ #rightarrow 4l MCFM', 'fnames': ['ggTo%s_Contin_MCFM701'%(fs) for fs in ('4e','2e2mu','4mu')]},
+    'qqZZ-EWK': {'title': 'q#bar{q} #rightarrow ZZjj EWK'    , 'fnames':['ZZTo4l_2Jets_EW']},
+    'qqZZ-int': {'title': 'q#bar{q} #rightarrow ZZjj interf.', 'fnames':['ZZTo4l_2Jets_EW_QCD']},
+    'qqZZ-QCD': {'title': 'q#bar{q} #rightarrow ZZjj QCD'    , 'fnames':['ZZTo4l_2Jets_QCD']},
+    'ggZZ'    : {'title': 'gg #rightarrow ZZ MCFM', 'fnames': ['ggTo%s_Contin_MCFM701'%(fs) for fs in ('4e','2e2mu','4mu')]},
     'rare'    : {'title': 'VVZ', 'fnames':['ZZZ','WZZ','WWZ']}, #ttZ "t#bar{t}Z" is missing!
-    'qqZZ-1J' : {'title': 'q#bar{q} #rightarrow ZZ+1j #rightarrow 4l j'        , 'fnames':['ZZTo4l_1Jets']},
-    'qqZZ'    : {'title': 'q#bar{q} #rightarrow ZZ #rightarrow 4l (Powheg)'    , 'fnames':['ZZTo4l']},
+    'qqZZ-1J' : {'title': 'q#bar{q} #rightarrow ZZ+1j'       , 'fnames':['ZZTo4l_1Jets']},
+    'qqZZ'    : {'title': 'q#bar{q} #rightarrow ZZ Powheg'   , 'fnames':['ZZTo4l']},
     'ZX'      : {'title': 'Z+X'                                                , 'fnames':['ZX']},
 }
 
